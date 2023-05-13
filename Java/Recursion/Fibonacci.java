147f5ee9aeff1730a972b93cb01f9bc6cccf6147
Abhishek Sharma <abhisheksharma@Abhisheks-MacBook-Pro.local>
2023-05-13 16:41:24 +0530

    added tracking of in progress operations

--- Java/Recursion/Fibonacci.java
+++ Java/Recursion/Fibonacci.java
@@ -13,6 +13,8 @@
 
 class Fibonacci {
     static int count;
+    static int inprogressOperations;
+    static int maxCount;
     static int fibo(int x) {
         count++;
         System.out.println("calculating for x: " + x);
@@ -20,17 +22,24 @@ class Fibonacci {
             System.out.println("hit the base case for x: " + x);
             return x;
         }
+        inprogressOperations++;
+        // worst case space complexity => size of recusive stack
+        maxCount = Math.max(inprogressOperations, maxCount);
 
         int subAns = fibo(x-1) + fibo(x-2);
         System.out.println("calculated for x: " + x + " ans: " + subAns);
+        inprogressOperations--;
         return subAns;
     }
 
     public static void main(String[] args) {
         int num = 5;
         count = 0;
+        inprogressOperations = 0;
+        maxCount = 0;
         int ans = fibo(num);
         System.out.println("fibonacci of " + num + " is: " + ans);
         System.out.println("Number of operations: " + count);
+        System.out.println("Max number of inprogress calculations: " + maxCount);
     }
 }
--- Java/Recursion/Tribonacci.java
+++ Java/Recursion/Tribonacci.java
@@ -7,6 +7,8 @@
 
 class Tribonacci {
     static int count;
+    static int inprogressOperations;
+    static int maxCount;
     static int tribo(int x) {
         count++;
         System.out.println("calculating for x: " + x);
@@ -14,18 +16,25 @@ class Tribonacci {
             System.out.println("hit the base case for x: " + x);
             return x;
         }
+        inprogressOperations++;
+        // worst case space complexity => size of recusive stack
+        maxCount = Math.max(inprogressOperations, maxCount);
 
         int subAns = tribo(x-1) + tribo(x-2) + tribo(x-3);
         System.out.println("calculated for x: " + x + " ans: " + subAns);
+        inprogressOperations--;
         return subAns;
     }
 
     public static void main(String[] args) {
         int num = 5;
         count = 0;
+        inprogressOperations = 0;
+        maxCount = 0;
         int ans = tribo(num);
         System.out.println("tribonacci of " + num + " is: " + ans);
         System.out.println("Number of operations: " + count);
+        System.out.println("Max number of inprogress calculations: " + maxCount);
     }
 }
 
